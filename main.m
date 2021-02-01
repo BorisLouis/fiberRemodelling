dc6925d38afed156576b9f50847e6b292c1174b0
DESKTOP-RTJSFL7\Boris <boris.louis999@gmail.com>
2021-02-01 11:09:23 +0100

    minor changes

--- main.m
+++ main.m
@@ -2,7 +2,7 @@ clear;
 close all;
 clc;
 %% User Input
-file.path = 'D:\Documents\Unif\PhD\2020-Data\07- July\MAX\Example data for Boris_1k_10nm_TFM\T0';
+file.path = 'D:\Documents\Unif\PhD\2020-Data\07- July\MAX\Example data for Boris_1k_10nm_TFM\T4';
 file.ext  = '.tif';
 
 info.pxSizeXY = 568; 
@@ -29,12 +29,12 @@ stack.plotCellContour();
 
 %% 3D
 %this step takes time so we don't really run it
-stack.renderCell3D();
+%stack.renderCell3D();
 
 %% Intensity analysis
 weight = [info.pxSizeXY,info.pxSizeXY,info.pxSizeZ];
 
-step = 200;
+step = 2*info.pxSizeXY;
 %test = DistMap.calcWeightedDistMap(Mask,weight);
 stack.intensityDistrib(weight,step);
 
